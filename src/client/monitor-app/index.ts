071d699d145bf0c3192149fb5906f0bc2dee2fb3
m.sakamaki <mail@m-sakamaki.jp>
2016-03-15 16:23:03 +0900

    add ga

--- src/client/monitor-app/index.ts
+++ src/client/monitor-app/index.ts
@@ -1,8 +1,39 @@
+declare var ga: any;
 
 console.log('start')
 
+const btn1: HTMLButtonElement = <HTMLButtonElement>document.querySelector('#run_exeption1');
+const btn2: HTMLButtonElement = <HTMLButtonElement>document.querySelector('#run_exeption2');
+
+btn1.onclick = () => {
+  var x: any = {};
+  try {
+    x['too']();
+  } catch (err) {
+    console.error(err);
+    ga('send', 'exception', {
+      'exDescription': err.message,
+      'exFatal': false
+    });
+  }
+}
+
+btn2.onclick = () => {
+  var x: any = {};
+  x['too']();
+}
+
+window.onerror = function(message) {
+  console.log('windows on error', message);
+    ga('send', 'exception', {
+      'exDescription': message,
+      'exFatal': false
+    });
+};
+
+
 class Hoge {
-  constructor() {}
+  constructor() { }
   say() {
     console.log('Hello');
   }
